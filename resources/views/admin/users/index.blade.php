b635901802ce6feddd3b94fe849250243ba1a5b7
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-25 14:23:34 -0300

    adding user index

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,92 @@
+@extends('adminlte::page')
+
+@section('title', config('app.name', 'SSV') )
+
+@section('content_header')
+    <h1 class="m-0 text-dark">{{ __('Activities') }}</h1>
+@stop
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <a href="{{ route('admin.users.create') }}" class="btn btn-primary"><i class="fas fa-plus"></i> {{ __('Add Activity') }}</i></a>
+                    <div class="card-tools">
+                      <div class="input-group input-group-sm">
+                        <input type="text" name="table_search" class="form-control float-right input-search" placeholder="{{ __('Search') }}">
+                        <div class="input-group-append">
+                          <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <div class="card-body table-responsive">
+                    <table class="table table-hover table-head-fixed text-nowrap table-search">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>{{ __('Name') }}</th>
+                                <th>{{ __('Actions') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td><a href="{{ route('admin.users.show', ['user' => $user->slug]) }}">{{$user->id}}</a></td>
+                                    <td><a href="{{ route('admin.users.show', ['user' => $user->slug]) }}">{{$user->name}}</a></td>
+                                    <td width="15%">
+                                        <div class="btn-group">
+                                            <a href="{{ route('admin.users.show', ['user' => $user->slug]) }}" class="btn btn-secondary btn-sm">
+                                                <i class="fas fa-book"></i> {{ __('details') }}</i>
+                                            </a>
+                                        </div>
+                                        <div class="btn-group">
+                                        <a href="{{ route('admin.users.edit', ['user' => $user->slug]) }}" class="btn btn-secondary btn-sm">
+                                                <i class="fas fa-pencil-alt"></i> {{ __('edit') }}</i>
+                                            </a>
+                                        </div>
+                                        <div class="btn-group">
+                                            <a href="#" class="btn btn-danger btn-sm delete-modal-click" data-toggle="modal" data-target="#delete-modal" data-id={{ $user->id }}>
+                                                <i class="fas fa-trash-alt"></i> {{ __('delete') }}</i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">{{ __('Do you really want to delete this item?') }}</div>
+                <div class="modal-footer">
+                    <form action="{{route('admin.users.destroy', ['user' => '#'])}}" method="post" id="delete-modal-form">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-primary">{{ __('Yes') }}</button>
+                    </form>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('No') }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('js')
+    <script src="{{ mix('js/list.js') }}"></script>
+@stop
+
+
+
