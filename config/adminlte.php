66800833978f5091abcb6b07ae27feb9f26225e3
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-28 13:45:10 -0300

    adding logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,8 +46,8 @@ return [
     */
 
     'logo' => '<b>Painel</b> SSV',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img' => 'vendor/adminlte/dist/img/VISOMES_LOGO.jpg',
+    'logo_img_class' => 'brand-image elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'SSV',
