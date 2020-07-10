c3401c358e5a7a7e4a079037ae1cf6ab304d6b5f
walfrido-oliveira <walfrido_15@hotmail.com>
2020-07-10 13:53:10 -0300

    adding automatic year

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-6">
-        <strong>Copyright © 20209 <a href="{{ url('/') }}">{{  config('app.name', 'SSV') }}</a>.</strong> All rights reserved.
+        <strong>Copyright © {{ date('Y') }} <a href="{{ url('/') }}">{{  config('app.name', 'SSV') }}</a>.</strong> All rights reserved.
     </div>
     <div class="col-6 text-right">
         <b>Version</b> {{ env('APP_VERSION', '') }}
