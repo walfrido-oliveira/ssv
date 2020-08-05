c8d92dc80f486bf2998c70d6c6e56759e421cb59
walfrido-oliveira <walfrido_15@hotmail.com>
2020-08-05 15:15:27 -0300

    adding order js file

---
+++ resources/js/order.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $('select[name=client_id]').select2({
+        language: "pt-BR",
+        ajax: {
+            url: '/admin/clients/find',
+            dataType: 'json',
+            data: function (params) {
+                var query = {
+                    q: $.trim(params.term),
+                    page: params.page
+                }
+                return query;
+            },
+            processResults: function (data, params) {
+                params.page = params.page || 1;
+
+                return {
+                    results: data,
+                    pagination: {
+                        more: (params.page * 30) < data.total_count
+                    }
+                };
+            },
+            cache: true
+        }
+    });
+
+    $('select[name=budget_id]').select2({
+        language: "pt-BR",
+        ajax: {
+            url: '/admin/budgets/find',
+            dataType: 'json',
+            data: function (params) {
+                var query = {
+                    q: $.trim(params.term),
+                    client_id: $("select[name='client_id'").val(),
+                    page: params.page
+                }
+                return query;
+            },
+            processResults: function (data, params) {
+                params.page = params.page || 1;
+
+                return {
+                    results: data,
+                    pagination: {
+                        more: (params.page * 30) < data.total_count
+                    }
+                };
+            },
+            cache: true
+        }
+    });
+
+    $("select[name='client_id'").on('change', function() {
+
+    });
+
+});
