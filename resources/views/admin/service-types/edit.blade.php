39d5528440ea8dfe2f0c956345fb54b3b20ada62
walfrido-oliveira <walfrido_15@hotmail.com>
2020-08-05 13:29:43 -0300

    adding service type views

---
+++ resources/views/admin/service-types/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('adminlte::page')
+
+@section('title', config('app.name', 'SSV') )
+
+@section('content_header')
+<h1 class="m-0 text-dark">{{ __('Edit Service Type') }}</h1>
+@stop
+
+@section('content')
+
+    <form action="{{ route('admin.service-types.update', ['transport_method' => $transportMethod->id]) }}" method="POST" enctype="multipart/form-data">
+
+        @csrf
+        @method("PUT")
+
+        <div class="content">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">{{ __('Name') }}</label>
+                                {!! Form::text('name', $transportMethod->name, ['class' => 'form-control ' . $errors->first('name','is-invalid'), 'id' => 'name', 'placeholder' => __("Name")]) !!}
+                                {!! $errors->first('name','<div class="invalid-feedback">:message</div>') !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 card-footer">
+                    <a href="{{ route('admin.service-types.index') }}" class="btn btn-secondary">{{ __('Cancel') }}</a>
+                    <input type="submit" value="{{ __('Confirm') }}" class="btn btn-primary float-right">
+                </div>
+            </div>
+        </div>
+    </form>
+@stop
+
+@section('js')
+@stop
+
