3b30f69128563bdc0751854f51f83d6ae6eaa4cd
walfrido-oliveira <walfrido_15@hotmail.com>
2020-08-11 10:15:28 -0300

    adding billing table

---
+++ database/migrations/2020_08_11_094351_create_billings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('budget_id');
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('payment_method_id');
+
+            $table->timestamp('due_date');
+            $table->timestamps();
+            $table->decimal('amount', 10, 2);
+            $table->enum('status', ['pending', 'overdue', 'paid'])->default('pending');
+
+            $table->foreign('budget_id')->references('id')->on('budgets');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}
