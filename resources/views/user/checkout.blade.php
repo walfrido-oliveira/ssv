f11b94547e365d18c0032dba49b254a46dc48c77
walfrido-oliveira <walfrido_15@hotmail.com>
2020-08-12 11:32:21 -0300

    adding messagens to error codes

--- resources/views/user/checkout.blade.php
+++ resources/views/user/checkout.blade.php
@@ -28,84 +28,70 @@
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="description">{{ __('Description') }}</label>
-                                    {!! Form::textarea('description','ok', ['class' => 'form-control ' . $errors->first('name','is-invalid'),
-                                    'id' => 'description', 'placeholder' => __("Description"), 'readonly']) !!}
-                                    {!! $errors->first('description','<div class="invalid-feedback">:message</div>') !!}
+                                    {!! Form::textarea('description','ok', ['class' => 'form-control ',
+                                    'id' => 'description', 'placeholder' => __("Description"), 'readonly', 'rows' => '3']) !!}
+                                    <div class="invalid-feedback d-block" id="invalid-description"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="transaction_amount">{{ __('Amount') }}</label>
-                                    {!! Form::text('transaction_amount', alternative_money($billing->budget->amount, '$', 2, ',', '.'), ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::text('transaction_amount', alternative_money($billing->budget->amount, '$', 2, ',', '.'),
+                                    ['class' => 'form-control ',
                                     'id' => 'transaction_amount', 'placeholder' => __("Amount"), 'readonly']) !!}
-                                    {!! $errors->first('transaction_amount','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-transaction_amount"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="cardNumber">{{ __('Card Number') }}</label>
-                                    {!! Form::text('cardNumber', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::text('cardNumber', null, ['class' => 'form-control ',
                                     'id' => 'cardNumber', 'placeholder' => __("Card Number"), 'data-checkout' => 'cardNumber',
                                     'onselectstart' => 'return false', 'onpaste' => 'return false', 'onCopy' => 'return false',
                                     'onCut' => 'return false', 'onDrag' => 'return false', 'onDrop' => 'return false',
                                     'autocomplete' => 'off']) !!}
-                                    {!! $errors->first('cardNumber','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-cardNumber"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="cardholderName">{{ __('Card Holder Name') }}</label>
-                                    {!! Form::text('cardholderName', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::text('cardholderName', null, ['class' => 'form-control ',
                                     'id' => 'cardholderName', 'placeholder' => __("Card Holder Name"), 'data-checkout' => 'cardholderName']) !!}
-                                    {!! $errors->first('cardholderName','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-cardholderName"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="cardExpirationMonth">{{ __('Card Expiration Month') }}</label>
-                                    {!! Form::text('cardExpirationMonth', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::text('cardExpirationMonth', null, ['class' => 'form-control ',
                                     'id' => 'cardExpirationMonth', 'placeholder' => __("Card Expiration Month"), 'data-checkout' => 'cardExpirationMonth',
                                     'onselectstart' => 'return false', 'onpaste' => 'return false', 'onCopy' => 'return false',
                                     'onCut' => 'return false', 'onDrag' => 'return false', 'onDrop' => 'return false',
                                     'autocomplete' => 'off']) !!}
-                                    {!! $errors->first('cardExpirationMonth','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-cardExpirationMonth"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="cardExpirationYear">{{ __('Card Expiration Year') }}</label>
-                                    {!! Form::text('cardExpirationYear', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::text('cardExpirationYear', null, ['class' => 'form-control ',
                                     'id' => 'cardExpirationYear', 'placeholder' => __("Card Expiration Year"), 'data-checkout' => 'cardExpirationYear',
                                     'onselectstart' => 'return false', 'onpaste' => 'return false', 'onCopy' => 'return false',
                                     'onCut' => 'return false', 'onDrag' => 'return false', 'onDrop' => 'return false',
                                     'autocomplete' => 'off']) !!}
-                                    {!! $errors->first('cardExpirationYear','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-cardExpirationYear"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="securityCode">{{ __('Security Code') }}</label>
-                                    {!! Form::text('securityCode', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::text('securityCode', null, ['class' => 'form-control ',
                                     'id' => 'securityCode', 'placeholder' => __("Security Code"), 'data-checkout' => 'securityCode',
                                     'onselectstart' => 'return false', 'onpaste' => 'return false', 'onCopy' => 'return false',
                                     'onCut' => 'return false', 'onDrag' => 'return false', 'onDrop' => 'return false',
                                     'autocomplete' => 'off']) !!}
-                                    {!! $errors->first('securityCode','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-securityCode"></div>
                                 </div>
                                 <div class="form-group">
                                     <label for="installments">{{ __('Installments') }}</label>
-                                    {!! Form::select('installments', [], null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
+                                    {!! Form::select('installments', [], null, ['class' => 'form-control ',
                                     'id' => 'installments', 'placeholder' => __("Installments")]) !!}
-                                    {!! $errors->first('installments','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-installments"></div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="docType">{{ __('Doc Type') }}</label>
-                                    {!! Form::select('docType', $docTypes, null,
-                                    ['class' => 'form-control ' . $errors->first('name','is-invalid'),
-                                    'id' => 'docType', 'placeholder' => __("Doc Type"), 'data-checkout' => 'docType']) !!}
-                                    {!! $errors->first('docType','<div class="invalid-feedback">:message</div>') !!}
-                                </div>
-                                <div class="form-group">
-                                    <label for="docNumber">{{ __('Doc Number') }}</label>
-                                    {!! Form::text('docNumber', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
-                                    'id' => 'docNumber', 'placeholder' => __("Doc Number"), 'data-checkout' => 'docNumber']) !!}
-                                    {!! $errors->first('docNumber','<div class="invalid-feedback">:message</div>') !!}
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">{{ __('Email') }}</label>
-                                    {!! Form::email('email', null, ['class' => 'form-control ' . $errors->first('name','is-invalid'),
-                                    'id' => 'email', 'placeholder' => __("Email")]) !!}
-                                    {!! $errors->first('email','<div class="invalid-feedback">:message</div>') !!}
+                                    <div class="invalid-feedback d-block" id="invalid-card-token"></div>
                                 </div>
                                 {!! Form::hidden('payment_method_id', null, ['id' => 'payment_method_id']) !!}
+                                {!! Form::hidden('billing_id', $billing->id, ['id' => 'billing_id']) !!}
                             </div>
                         </div>
                     </div>
@@ -119,6 +105,7 @@
             </div>
         </div>
     </form>
+
 @stop
 
 @section('js')
@@ -148,7 +135,8 @@
                 element.value = paymentMethodId;
                 getInstallments();
             } else {
-                alert(`payment method info error: ${response}`);
+                let invalidCardnumber = document.getElementById("invalid-cardNumber");
+                invalidCardnumber.innerHTML = response;
             }
         }
 
@@ -167,7 +155,8 @@
                         document.getElementById('installments').appendChild(opt);
                     });
                 } else {
-                    alert(`installments method info error: ${response}`);
+                    let invalidCardnumber = document.getElementById("invalid-cardNumber");
+                    invalidCardnumber.innerHTML = response;
                 }
             });
         }
@@ -188,7 +177,7 @@
 
         function sdkResponseHandler(status, response) {
             if (status != 200 && status != 201) {
-                alert("verify filled data");
+                setCodeErrorResponse(response.cause);
             }else{
                 var form = document.querySelector('#pay');
                 var card = document.createElement('input');
@@ -200,6 +189,105 @@
                 form.submit();
             }
         };
+
+        function setCodeErrorResponse(cause) {
+            var invalidCardnumber = document.getElementById("invalid-cardNumber");
+            var cardnumber = document.getElementById("cardNumber");
+
+            var invalidCardExpirationMonth  = document.getElementById("invalid-cardExpirationMonth");
+            var cardExpirationMonth  = document.getElementById("cardExpirationMonth");
+
+            var invalidCardExpirationYear = document.getElementById("invalid-cardExpirationYear");
+            var cardExpirationYear = document.getElementById("cardExpirationYear");
+
+            var invalidCardholderName = document.getElementById("invalid-cardholderName");
+            var cardholderName = document.getElementById("cardholderName");
+
+            var invalidSecurityCode = document.getElementById("invalid-securityCode");
+            var securityCode = document.getElementById("securityCode");
+
+            var invalidCardToken = document.getElementById("invalid-card-token");
+
+            var paymentMethodId = document.getElementById("payment_method_id").value;
+
+            cause.forEach(element => {
+                switch (element.code) {
+                    case '205':
+                        invalidCardnumber.innerHTML = 'Digite o número do seu cartão.';
+                        cardnumber.classList.add('is-invalid');
+                        break;
+                    case '208':
+                        invalidCardExpirationMonth.innerHTML = 'Escolha um mês.';
+                        cardExpirationMonth.classList.add('is-invalid');
+                        break;
+                    case '209':
+                        invalidCardExpirationYear.innerHTML = 'Escolha um ano.';
+                        cardExpirationYear.classList.add('is-invalid');
+                        break;
+                    case '221':
+                        invalidCardholderName.innerHTML = 'Digite o nome e sobrenome.';
+                        cardholderName.classList.add('is-invalid');
+                        break;
+                    case '224':
+                        invalidSecurityCode.innerHTML = 'Digite o código de segurança.';
+                        securityCode.classList.add('is-invalid');
+                        break;
+                    case 'E301':
+                        invalidCardnumber.innerHTML = 'Digite o número do seu cartão.';
+                        cardnumber.classList.add('is-invalid');
+                        break;
+                    case 'E302':
+                        invalidSecurityCode.innerHTML = 'Confira o código de segurança.';
+                        securityCode.classList.add('is-invalid');
+                        break;
+                    case '316':
+                        invalidCardholderName.innerHTML = 'Por favor, digite um nome válido.';
+                        cardholderName.classList.add('is-invalid');
+                        break;
+                    case '325':
+                        invalidCardExpirationMonth.innerHTML = 'Confira a data.';
+                        cardExpirationMonth.classList.add('is-invalid');
+                        break;
+                    case '326':
+                        invalidCardExpirationYear.innerHTML = 'Escolha um ano.';
+                        cardExpirationYear.classList.add('is-invalid');
+                        break;
+                    case '106':
+                        invalidCardToken.innerHTML = 'Não pode efetuar pagamentos a usuários de outros países.';
+                        break;
+                    case '109':
+                        invalidCardToken.innerHTML = 'O ${paymentMethodId} não processa pagamentos parcelados. Escolha outro cartão ou outra forma de pagamento.';
+                        break;
+                    case '126':
+                        invalidCardToken.innerHTML = 'Não conseguimos processar seu pagamento.';
+                        break;
+                    case '129':
+                        invalidCardToken.innerHTML = 'O ${paymentMethodId} não processa pagamentos para o valor selecionado. Escolha outro cartão ou outra forma de pagamento.';
+                        break;
+                    case '145':
+                        invalidCardToken.innerHTML = 'Uma das partes com a qual está tentando realizar o pagamento é um usuário de teste e a outra é um usuário real.';
+                        break;
+                    case '150':
+                        invalidCardToken.innerHTML = 'Você não pode efetuar pagamentos.';
+                        break;
+                    case '151':
+                        invalidCardToken.innerHTML = 'Você não pode efetuar pagamentos.';
+                        break;
+                    case '160':
+                        invalidCardToken.innerHTML = 'Não conseguimos processar seu pagamento.';
+                        break;
+                    case '204':
+                        invalidCardToken.innerHTML = 'O ${paymentMethodId} não está disponível nesse momento. Escolha outro cartão ou outra forma de pagamento.';
+                        break;
+                    case '801':
+                        invalidCardToken.innerHTML = 'Você realizou um pagamento similar há poucos instantes. Tente novamente em alguns minutos.';
+                        break;
+                    case 'default':
+                        invalidCardToken.innerHTML = 'Não pudemos processar seu pagamento.';
+                        break;
+                }
+            });
+        }
     </script>
 
 @stop
